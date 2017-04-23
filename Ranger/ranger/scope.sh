930ee99cbbd85bc3cec1cb31e8784a43e8c8ffbd
elsuizo <martin.noblia@openmailbox.org>
2017-04-23 12:30:17 -0300

    update the new config :beer:

--- Ranger/ranger/scope.sh
+++ Ranger/ranger/scope.sh
@@ -118,4 +118,12 @@ case "$mimetype" in
         try mediainfo "$path" && { dump | trim | sed 's/  \+:/: /;';  exit 5; } || exit 1;;
 esac
 
+# Image previews, if enabled in ranger.
+if [ "$preview_images" = "True" ]; then
+    case "$mimetype" in
+        application/pdf)
+             pdftoppm -jpeg -singlefile "$path" "${cached//.jpg}" && exit 6;;
+    esac
+fi
+
 exit 1
