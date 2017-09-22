037a591aa0c7a8dcd0a76af26d787dbb4792a50e
elsuizo <mnoblia@disroot.org>
2017-09-22 00:30:55 -0300

    update repos

--- init_repos.sh
+++ init_repos.sh
@@ -5,7 +5,7 @@ git clone https://github.com/elsuizo/Control1.git
 git clone https://github.com/elsuizo/TODO_list.git
 git clone https://github.com/elsuizo/Poles_adventures.git
 git clone https://github.com/elsuizo/Objects_detection.git
-git clone https://github.com/elsuizo/opencv.git
+# git clone https://github.com/elsuizo/opencv.git
 git clone https://github.com/elsuizo/C_work.git
 git clone https://github.com/elsuizo/AVR_programming.git
 git clone https://github.com/elsuizo/Haskell_work.git
@@ -19,16 +19,16 @@ git clone https://github.com/elsuizo/Kivy_work.git
 git clone https://github.com/elsuizo/Rust_work.git
 git clone https://github.com/elsuizo/Cpp.git
 git clone https://github.com/elsuizo/Telecomunicaciones.git
-git clone https://github.com/elsuizo/Rosette.git
+# git clone https://github.com/elsuizo/Rosette.git
 git clone https://github.com/elsuizo/Euler.git
 git clone https://github.com/elsuizo/Numerical_methods.git
 git clone https://github.com/elsuizo/Image_processing.git
 git clone https://github.com/elsuizo/SFML_work.git
-git clone https://github.com/elsuizo/Cpp_real_time.git
+# git clone https://github.com/elsuizo/Cpp_real_time.git
 git clone https://github.com/elsuizo/Nonlinear_systems.git
 git clone https://github.com/elsuizo/Python_work_git.git
 git clone https://github.com/elsuizo/Practicas_vision.git
-git clone https://github.com/elsuizo/elsuizo.github.io.git
+# git clone https://github.com/elsuizo/elsuizo.github.io.git
 git clone https://github.com/elsuizo/Optical_flow.git
 git clone https://github.com/elsuizo/Charlas_presentaciones.git
 git clone ttps://github.com/elsuizo/Control_de_robots_py.git
@@ -37,3 +37,8 @@ git clone https://github.com/elsuizo/Redes_fuzzy.git
 git clone https://github.com/elsuizo/Fortran_work.git
 git clone https://github.com/elsuizo/K-vector.git
 git clone https://github.com/elsuizo/Beaglebone_opencv.git
+git clone https://github.com/elsuizo/zerial.git
+git clone https://github.com/elsuizo/C_gui_template.git
+git clone https://github.com/elsuizo/C_utils.git
+git clone https://github.com/elsuizo/Arduino_work.git
+git clone https://github.com/elsuizo/RSIS_Rust.git
