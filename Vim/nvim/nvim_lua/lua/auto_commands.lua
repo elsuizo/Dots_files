ee9316eb06edee94a921401504669e31d905ba6e
elsuizo <mnoblia@disroot.org>
2021-05-01 11:39:04 -0300

    upate la config con lua

---
+++ Vim/nvim/nvim_lua/lua/auto_commands.lua
@@ -0,0 +1,49 @@
+local M = {}
+function M.autocmd(event, triggers, operations)
+  local cmd = string.format("autocmd %s %s %s", event, triggers, operations)
+ vim.cmd(cmd)
+end
+
+M.autocmd("BufEnter",     "*",   "if &buftype == 'terminal' | :startinsert | endif")
+M.autocmd("BufReadPost",  "*",   [[if line("'\"") > 0 && line ("'\"") <= line("$") | exe "normal! g'\"" | endif]])
+M.autocmd("BufWritePre",  "*",   "%s/\\s\\+$//e")
+M.autocmd("BufRead",  	  "*",   "normal zz")
+
+-- M.autocmd("ColorScheme",  "*",   "lua require('mh.colors').setItalics()")
+-- M.autocmd("CompleteDone", "*",   "pclose")
+
+-- M.autocmd("FileType",     "vue", "syntax sync fromstart")
+M.autocmd("FileType",     "rust", "nmap gd <Plug>(rust-def)")
+M.autocmd("FileType",     "rust", "nmap gs <Plug>(rust-def-split)")
+M.autocmd("FileType",     "rust", "nmap gx <Plug>(rust-def-vertical)")
+M.autocmd("FileType",     "rust", "nmap <leader>gd <Plug>(rust-doc)")
+M.autocmd("FileType",     "rust", "nmap <leader>r :Cargo run<cr>")
+M.autocmd("FileType",     "rust", "nmap <leader>t :Cargo test<cr>")
+M.autocmd("FileType",     "rust", "nmap <leader>tc :Cargo check<cr>")
+
+-- TODO(elsuizo) esto no se para que es
+-- M.autocmd("InsertEnter",  "*",   "let save_cwd = getcwd() | set autochdir")
+-- M.autocmd("InsertEnter",  "*",   "if !exists('w:last_fdm') | let w:last_fdm=&foldmethod | setlocal foldmethod=manual | endif")
+--
+-- M.autocmd("InsertLeave",  "*",   "set noautochdir | execute 'cd' fnameescape(save_cwd)")
+-- M.autocmd("InsertLeave",  "*",   "if exists('w:last_fdm') | let &l:foldmethod=w:last_fdm | unlet w:last_fdm | endif")
+
+M.autocmd("TermOpen",     "*",   "setl bufhidden=hide")
+M.autocmd("TermOpen",     "*",   "startinsert")
+M.autocmd("TermOpen",     "*",   "setl nonumber")
+
+M.autocmd("WinLeave",     "*",   "if exists('w:last_fdm') | let &l:foldmethod=w:last_fdm | unlet w:last_fdm | endif")
+
+
+-- vim.api.nvim_command('autocmd WinEnter * v:lua.mh.autocmds.Preview_func()')
+
+-- autocmd WinEnter * call Preview_func()
+--
+-- function! Preview_func()
+--   if &pvw
+--     setlocal nonumber norelativenumber
+--    endif
+-- endfunction
+--
+--
+return M
---
+++ Vim/nvim/nvim_lua/lua/variables.lua
@@ -0,0 +1,13 @@
+-- global variables
+vim.api.nvim_set_var('clipboard', 
+  [[\ 'name': 'xclip-xfce4-clipman',
+  \   'copy': {
+  \      '+': 'xclip -selection clipboard',
+  \      '*': 'xclip -selection clipboard',
+  \    },
+  \   'paste': {
+  \      '+': 'xclip -selection clipboard -o',
+  \      '*': 'xclip -selection clipboard -o',
+  \   },
+  \   'cache_enabled': 1,
+  \ ]])
