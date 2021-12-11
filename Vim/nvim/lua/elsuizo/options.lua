b495c9fb54d56b41c168feff26503aeef0067420
elsuizo <mnoblia@disroot.org>
2021-12-11 19:40:22 -0300

    update telescope clipboard configuration

--- Vim/nvim/lua/elsuizo/options.lua
+++ Vim/nvim/lua/elsuizo/options.lua
@@ -137,6 +137,32 @@ vim.g.move_key_modifier = 'C'
 
 -- vim.o.completeopt = "menuone,noselect"
 
+require('neoclip').setup({
+      history = 1000,
+      enable_persistant_history = false,
+      db_path = vim.fn.stdpath("data") .. "/databases/neoclip.sqlite3",
+      filter = nil,
+      preview = true,
+      default_register = '"',
+      content_spec_column = false,
+      on_paste = {
+        set_reg = false,
+      },
+      keys = {
+        i = {
+          select = '<cr>',
+          paste = '<c-p>',
+          paste_behind = '<c-k>',
+          custom = {},
+        },
+        n = {
+          select = '<cr>',
+          paste = 'p',
+          paste_behind = 'P',
+          custom = {},
+        },
+      },
+})
 -- NOTE(elsuizo:2021-11-03): esto por ahora lo dejo por si me arrepiento de nvim-cmp
 -- require'compe'.setup {
 --   enabled = true;
