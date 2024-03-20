93935049a62ebb9338fcd2be77a8a3503d06c909
elsuizo <mnoblia@disroot.org>
2024-03-20 13:59:37 -0300

    update fix colorbuddy

--- Vim/nvim/lua/elsuizo/colors.lua
+++ Vim/nvim/lua/elsuizo/colors.lua
@@ -1,9 +1,7 @@
 -- el mejor colorscheme
 -- vim.cmd("colorscheme Monosvkem")
--- vim.cmd("colorscheme dracula-soft")
 -- vim.cmd("colorscheme paper")
--- vim.cmd("colorscheme envy")
--- vim.cmd("colorscheme nord")
+-- vim.cmd("colorscheme noirbuddy")
 --TODO(elsuizo: 2023-05-13): hacer esto en el colorscheme
 -- lsp colors setup
 vim.cmd [[
@@ -19,62 +17,62 @@ hi DiagnosticHint ctermbg=NONE ctermfg=NONE guibg=NONE guifg=#404040]]
 local LowEntropy = {}
 
 local colors = {
-  black        = "#121212",
-  white        = '#d7d7d7',
-  red          = '#fb4934',
-  green        = '#b8bb26',
-  blue         = '#0056b7',
-  gray         = '#a89984',
-  darkgray     = '#3c3836',
-  lightgray    = '#504945',
-  inactivegray = '#7c6f64',
+   black        = "#121212",
+   white        = '#d7d7d7',
+   red          = '#fb4934',
+   green        = '#b8bb26',
+   blue         = '#0056b7',
+   gray         = '#a89984',
+   darkgray     = '#3c3836',
+   lightgray    = '#504945',
+   inactivegray = '#7c6f64',
 }
 
 LowEntropy.normal = {
-  a = { bg = colors.white, fg = colors.black, gui = "bold", },
-  b = { bg = colors.blue, fg = colors.white, },
-  c = { bg = colors.black, fg = colors.white },
+   a = { bg = colors.white, fg = colors.black, gui = "bold", },
+   b = { bg = colors.blue, fg = colors.white, },
+   c = { bg = colors.black, fg = colors.white },
 }
 
 LowEntropy.insert = {
-  a = { bg = colors.blue, fg = colors.black, gui = "bold", },
-  b = { bg = colors.black, fg = colors.white },
-  c = { bg = colors.lightgray, fg = colors.white }
+   a = { bg = colors.blue, fg = colors.black, gui = "bold", },
+   b = { bg = colors.black, fg = colors.white },
+   c = { bg = colors.lightgray, fg = colors.white }
 }
 
 LowEntropy.visual = {
-  a = { bg = colors.white, fg = colors.black, gui = "bold", },
-  b = { bg = colors.blue, fg = colors.white, },
-  c = { bg = colors.lightgray, fg = colors.black },
+   a = { bg = colors.white, fg = colors.black, gui = "bold", },
+   b = { bg = colors.blue, fg = colors.white, },
+   c = { bg = colors.lightgray, fg = colors.black },
 }
 
 LowEntropy.replace = {
-  a = { bg = colors.white, fg = colors.black, gui = "bold", },
-  b = { bg = colors.lightgray, fg = colors.white, },
-  c = { bg = colors.black, fg = colors.white },
+   a = { bg = colors.white, fg = colors.black, gui = "bold", },
+   b = { bg = colors.lightgray, fg = colors.white, },
+   c = { bg = colors.black, fg = colors.white },
 }
 
 LowEntropy.command = {
-  a = { bg = colors.white, fg = colors.black},
-  b = { bg = colors.black, fg = colors.white, },
-  c = { bg = colors.lightgray, fg = colors.black },
+   a = { bg = colors.white, fg = colors.black },
+   b = { bg = colors.black, fg = colors.white, },
+   c = { bg = colors.lightgray, fg = colors.black },
 }
 
 LowEntropy.inactive = {
-  a = { bg = colors.black, fg = colors.white, gui = "bold", },
-  b = { bg = colors.black, fg = colors.white, },
-  c = { bg = colors.black, fg = colors.white, },
+   a = { bg = colors.black, fg = colors.white, gui = "bold", },
+   b = { bg = colors.black, fg = colors.white, },
+   c = { bg = colors.black, fg = colors.white, },
 }
 
 -- TODO(elsuizo:2021-11-08): hacer que nos diga la linea donde esta el trailing como en airline
 -- detecta cuando tenemos un trailing whitespace
 local function trailing()
-  return vim.fn.search([[\s\+$]], 'nw') ~= 0 and 'TW' or ''
+   return vim.fn.search([[\s\+$]], 'nw') ~= 0 and 'TW' or ''
 end
 
 -- lualine setup
 require('lualine').setup {
-   options = {
-      theme = LowEntropy,
-      }
+   -- options = {
+   --    theme = LowEntropy,
+   -- }
 }
--- Vim/nvim/lua/elsuizo/lazy_plugins.lua
+++ Vim/nvim/lua/elsuizo/lazy_plugins.lua
@@ -1,99 +1,103 @@
 -- lazy initialization
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+   vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", -- latest stable release
+      lazypath,
+   })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
    {
-     'stevearc/conform.nvim',                                                               -- formatting plugin
-     opts = {},
+      'stevearc/conform.nvim', -- formatting plugin
+      opts = {},
    },
-   'jiangmiao/auto-pairs',                                                                  -- smart autopairs
+   'jiangmiao/auto-pairs', -- smart autopairs
    'yorickpeterse/vim-paper',
    { 'projekt0n/github-nvim-theme' },
    -- 'JuliaEditorSupport/julia-vim',                                                       -- Julia language support
-   'fedepujol/move.nvim',                                                                   -- move blocks of code
+   'fedepujol/move.nvim', -- move blocks of code
    -- 'lervag/vimtex',                                                                         -- for work with tex files
-   'rust-lang/rust.vim',                                                                    -- Rust language support
+   'rust-lang/rust.vim',  -- Rust language support
    -- 'chrisbra/csv.vim',                                                                      -- Visualize csv files
    -- 'elsuizo/vim-colors-paramount-suizo',                                                    -- personal colorscheme
    -- 'neutaaaaan/iosvkem',                                                                    -- one of the best colorschemes
-   'tomtom/tcomment_vim',                                                                   -- comment lines or blocks of code
-   'elsuizo/monosvkem',                                                                     -- the best colorscheme
-   'roxma/vim-tmux-clipboard',                                                              -- to get nice clipboard interface with tmux panes
+   'tomtom/tcomment_vim',                      -- comment lines or blocks of code
+   'elsuizo/monosvkem',                        -- the best colorscheme
+   'roxma/vim-tmux-clipboard',                 -- to get nice clipboard interface with tmux panes
    -- 'alaric/neovim-visor',                                                                   -- open/close the terminal with Alt-t
-   'nvim-lua/plenary.nvim',                                                                 -- async support for lua
-   'nvim-telescope/telescope.nvim',                                                         -- fuzzy finder and more...
-   'nvim-telescope/telescope-fzy-native.nvim',                                              -- fuzzy finder and more...
-   'cljoly/telescope-repo.nvim',                                                            -- for finding stuff relative to a specific folder
-   {'romgrk/barbar.nvim',                                                                   -- bar to visualize buffers and tabs
-       dependencies = {
-         'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-       },
+   'nvim-lua/plenary.nvim',                    -- async support for lua
+   'nvim-telescope/telescope.nvim',            -- fuzzy finder and more...
+   'nvim-telescope/telescope-fzy-native.nvim', -- fuzzy finder and more...
+   'cljoly/telescope-repo.nvim',               -- for finding stuff relative to a specific folder
+   {
+      'romgrk/barbar.nvim',                    -- bar to visualize buffers and tabs
+      dependencies = {
+         'lewis6991/gitsigns.nvim',            -- OPTIONAL: for git status
+      },
    },
-   'ziglang/zig.vim',                                                                       -- zig language support
-   'neovim/nvim-lspconfig',                                                                 -- lsp neovim support
-   "L3MON4D3/LuaSnip",                                                                      -- snippet engine
-   "rafamadriz/friendly-snippets",                                                          -- a bunch of snippets to use
-   "saadparwaiz1/cmp_luasnip",                                                              -- snippet completions
-   "hrsh7th/cmp-nvim-lsp-signature-help",                                                   -- show function and methods signature
-   "hrsh7th/cmp-buffer" ,                                                                   -- autocomplete buffer words
-   "hrsh7th/cmp-nvim-lsp" ,                                                                 -- autocomplete lsp stuff
-   "hrsh7th/cmp-path" ,                                                                     -- autocomplete paths
-   "hrsh7th/cmp-nvim-lua" ,                                                                 -- autocomplete for lua
-   "tamago324/cmp-zsh" ,                                                                    ---autocomplete for zsh
-   "hrsh7th/cmp-cmdline" ,                                                                  -- autocomplete for neovim command line terminal
-   "ray-x/cmp-treesitter" ,                                                                 -- autocomplete treesitter support
-   "hrsh7th/nvim-cmp" ,                                                                     -- autocomplete engine
-   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+   'ziglang/zig.vim',                     -- zig language support
+   'neovim/nvim-lspconfig',               -- lsp neovim support
+   "L3MON4D3/LuaSnip",                    -- snippet engine
+   "rafamadriz/friendly-snippets",        -- a bunch of snippets to use
+   "saadparwaiz1/cmp_luasnip",            -- snippet completions
+   "hrsh7th/cmp-nvim-lsp-signature-help", -- show function and methods signature
+   "hrsh7th/cmp-buffer",                  -- autocomplete buffer words
+   "hrsh7th/cmp-nvim-lsp",                -- autocomplete lsp stuff
+   "hrsh7th/cmp-path",                    -- autocomplete paths
+   "hrsh7th/cmp-nvim-lua",                -- autocomplete for lua
+   "tamago324/cmp-zsh",                   ---autocomplete for zsh
+   "hrsh7th/cmp-cmdline",                 -- autocomplete for neovim command line terminal
+   "ray-x/cmp-treesitter",                -- autocomplete treesitter support
+   "hrsh7th/nvim-cmp",                    -- autocomplete engine
+   { "catppuccin/nvim",            name = "catppuccin", priority = 1000 },
    -- "windwp/nvim-ts-autotag",                                                             -- to close automagically tags
-   'tjdevries/colorbuddy.nvim',                                                             -- to create custom colorschemes
-   "numToStr/FTerm.nvim",                                                                   -- floating terminal
    {
-     'stevearc/aerial.nvim',
-     opts = {},
-     -- Optional dependencies
-     dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "nvim-tree/nvim-web-devicons"
-     },
+      'tjdevries/colorbuddy.nvim',
+      commit = "cdb5b0654d3cafe61d2a845e15b2b4b0e78e752a",
+   },                     -- to create custom colorschemes
+   "numToStr/FTerm.nvim", -- floating terminal
+   {
+      'stevearc/aerial.nvim',
+      opts = {},
+      -- Optional dependencies
+      dependencies = {
+         "nvim-treesitter/nvim-treesitter",
+         "nvim-tree/nvim-web-devicons"
+      },
    },
    {
-       "nvim-neorg/neorg",
-       -- tag = "*",
-       ft = "norg",
-       after = "nvim-treesitter", -- You may want to specify Telescope here as well
+      "nvim-neorg/neorg",
+      -- tag = "*",
+      ft = "norg",
+      after = "nvim-treesitter", -- You may want to specify Telescope here as well
    },
    {
-    'nvim-treesitter/nvim-treesitter',
-    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,   -- treesitter support
+      'nvim-treesitter/nvim-treesitter',
+      run = function() require('nvim-treesitter.install').update({ with_sync = true }) end, -- treesitter support
    },
    {
-     'nvim-tree/nvim-tree.lua',
-     requires = {
-       'nvim-tree/nvim-web-devicons',
-     },                                                                                     -- tree folder visualization
+      'nvim-tree/nvim-tree.lua',
+      requires = {
+         'nvim-tree/nvim-web-devicons',
+      }, -- tree folder visualization
    },
    {
-     'hoob3rt/lualine.nvim',
-     requires = {'kyazdani42/nvim-web-devicons', opt = true}                                -- state command visualization
+      'hoob3rt/lualine.nvim',
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true } -- state command visualization
    },
    -- -- databases stuff
-   "tpope/vim-dadbod",                                                                      -- database visualization
-   "tpope/vim-dotenv",                                                                      -- .env files support
+   "tpope/vim-dadbod", -- database visualization
+   "tpope/vim-dotenv", -- .env files support
    {
       'kristijanhusak/vim-dadbod-ui',
       requires = {
-         'tpope/vim-dadbod',                                                                -- database visualization
+         'tpope/vim-dadbod', -- database visualization
          'tpope/vim-dotenv',
       }
    },
